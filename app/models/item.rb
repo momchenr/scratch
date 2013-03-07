2db524438f97be8362ec44fcb90731d8f849be06
Mike McHenry <michaelomchenry@gmail.com>
2013-03-07 14:31:02 -0500

    add tire requires

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ActiveRecord::Base
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+
   attr_accessible :content, :user_id, :title
   validates :content, :length => { :maximum => 140 }
   belongs_to :user
