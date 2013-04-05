c6509b2cd4c56b09881eb723a1cab15f03c70847
Mike McHenry <michaelomchenry@gmail.com>
2013-04-04 21:31:02 -0400

    migrate it.

---
+++ db/migrate/20130405005015_add_image_to_items.rb
@@ -0,0 +1,8 @@
+class AddImageToItems < ActiveRecord::Migration
+  def self.up
+    add_attachment :items, :image
+  end
+  def self.down
+    remove_attachment :items, :image
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130321190003) do
+ActiveRecord::Schema.define(:version => 20130405005015) do
 
   create_table "items", :force => true do |t|
     t.string   "content"
     t.integer  "user_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.string   "title"
     t.string   "price"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "items", ["user_id", "created_at"], :name => "index_items_on_user_id_and_created_at"
