d99f16efb5856a3d306fee3aed54e52636fd3eee
Mike McHenry <michaelomchenry@gmail.com>
2013-06-03 16:24:35 -0400

    more copy changes, default email change

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "info@overflowapp.com"
+  default from: "overflowmech@gmail.com"
   
   def welcome_email(user)
     @user = user
     @url  = "http://www.overflowmech.com"
-    mail(:to => user.email, :subject => "Welcome to Overflow!")
+    mail(:to => user.email, :subject => "Welcome to OverflowMech!")
   end
 end
